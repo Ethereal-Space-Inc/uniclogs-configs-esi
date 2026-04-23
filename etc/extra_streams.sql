907c7f894f0906f4e45ae22638b68d5112669b8b
Hunter-McClellan <huntermcclellan0@gmail.com>
2026-04-23 15:49:32 -0600

    Bringing things closer to how the USLP implementation in an attempt to enable COP-1. Yamcs now queues AD frames and never sends them.

--- etc/extra_streams.sql
+++ etc/extra_streams.sql
@@ -1,20 +1,34 @@
-create stream cfdp_in as
-select
-    packet as pdu
-from
-    tmr_beacon create stream cfdp_out (
-        gentime TIMESTAMP,
-        entityId long,
-        seqNum int,
-        pdu binary
-    )
-insert into
-    cfdp_out
-select
-    gentime,
-    'cfdp-service' as origin,
-    seqNum,
-    '/yamcs/cfdp/upload' as cmdName,
-    pdu as binary
-from
-    cfdp_out
+-- create stream cfdp_in as
+-- select
+--     packet as pdu
+-- from
+--     tmr_beacon create stream cfdp_out (
+--         gentime TIMESTAMP,
+--         entityId long,
+--         seqNum int,
+--         pdu binary
+--     )
+-- insert into
+--     cfdp_out
+-- select
+--     gentime,
+--     'cfdp-service' as origin,
+--     seqNum,
+--     '/yamcs/cfdp/upload' as cmdName,
+--     pdu as binary
+-- from
+--     cfdp_out
+
+create table if not exists invalid_tm(rectime timestamp, seqNum long, packet binary, primary key(rectime, seqNum))
+insert into invalid_tm select * from invalid_tm_stream
+
+
+-- create the good and bad frame streams and tables. The definition has to match the one from org.yamcs.tctm.ccsds.FrameStreamHelper
+
+create stream good_frame_stream(rectime timestamp, seq int, ertime hres_timestamp, scid int, vcid int, data binary)
+create table if not exists good_frames(rectime timestamp, seq int, ertime hres_timestamp, scid int, vcid int, data binary, primary key(rectime, seq))
+insert into good_frames select * from good_frame_stream
+
+create stream bad_frame_stream(rectime timestamp, seq int, ertime hres_timestamp, data binary)
+create table if not exists bad_frames(rectime timestamp, seq int, ertime hres_timestamp, data binary, primary key(rectime, seq))
+insert into bad_frames select * from bad_frame_stream
